aeea90047f4794383518745e4f6ab5b00155abe8
emersonpessoa <eps011108@gmail.com>
2025-11-16 20:30:35 -0300

    feat: Criado Login de usuário :rocket:

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,36 @@
+// Login de usuário
 const myModal = new bootstrap.Modal(document.getElementById("register-modal"));
+let logged = sessionStorage.getItem("logged");
+const session = localStorage.getItem("session");
+
+// Verificar se está logado
+checkLogged();
+
+document.getElementById("login-form").addEventListener("submit", function (event) {
+  event.preventDefault();
+  const email = document.getElementById("email-input").value;
+  const password = document.getElementById("password-input").value;
+  const checkSession = document.getElementById("session-check").checked;
+
+  // console.log(email,password,session);
+
+  //  recuperar os dados do localStorage
+  const account = getAccount(email);
+
+  if (!account) {
+    alert("Ops! Verifique o seu usuário ou senha.");
+    return;
+  }
+  // validar usuario e senha
+  if (account.login !== email || account.password !== password) {
+    alert("Ops! Verifique o seu usuário ou senha.");
+    return;
+  }
+  saveSession(email, checkSession);
+  window.location.href = "home.html";
+});
+
+//Criar conta
 
 document.getElementById("create-form").addEventListener("submit", function (event) {
   event.preventDefault();
@@ -14,6 +46,8 @@ document.getElementById("create-form").addEventListener("submit", function (even
     alert("A senha deve ter no mínimo 4 caracteres");
     return;
   }
+  
+
   saveAccount({
     login: email,
     password: password,
@@ -24,8 +58,40 @@ document.getElementById("create-form").addEventListener("submit", function (even
   alert(`Conta criada com sucesso!\nEmail: ${email}`);
 });
 
+//Checar se está logado
+  function checkLogged() {
+    if (session) {
+      sessionStorage.setItem("logged", session);
+      logged = session;
+    }
+    if (logged) {
+        saveSession(logged, session);
+        window.location.href = "home.html";
+    }
+  }
+
+// Salvar dados da conta
 function saveAccount(data) {
   //   console.log(data);
   //  salvar os dados no localStorage
   localStorage.setItem(data.login, JSON.stringify(data));
+}
+// Salvar sessão
+function saveSession(data, saveSession) {
+  if (saveSession) {
+    localStorage.setItem("session", data);
+  } else {
+    sessionStorage.setItem("logged", data);
+  }
+}
+
+// Obter dados da conta
+function getAccount(key) {
+  const account = localStorage.getItem(key);
+
+  if (account) {
+    //  converter de volta para objeto
+    return JSON.parse(account);
+  }
+  return "";
 }
