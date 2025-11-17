6fe3e29bc5fbc2b95fa2c1606ccf294510121f84
emersonpessoa <eps011108@gmail.com>
2025-11-17 13:08:04 -0300

    feat: Verifica se tem usuário logado, caso não, manda para index.hml :sparker:

---
+++ public/js/home.js
@@ -0,0 +1,19 @@
+// Cadastrar usuário
+const myModal = new bootstrap.Modal(document.getElementById("transaction-modal"));
+let logged = sessionStorage.getItem("logged");
+const session = localStorage.getItem("session");
+
+checkLogged();
+
+// Verificar se está logado, caso não esteja, redirecionar para index.html
+
+function checkLogged() {
+  if (session) {
+    sessionStorage.setItem("logged", session);
+    logged = session;
+  }
+  if (!logged) {
+    window.location.href = "index.html";
+    return;
+  }
+}
