a451025ab97049212792234e892718d16c5b17f8
emersonpessoa <eps011108@gmail.com>
2025-11-17 20:12:07 -0300

    feat: Criado função para trazer as entradas do tipo 1 :sparkler:

--- public/js/home.js
+++ public/js/home.js
@@ -6,7 +6,6 @@ let data = {
   transactions: [],
 };
 
-
 const logout = document.getElementById("button-logout");
 
 checkLogged();
@@ -19,8 +18,6 @@ document.getElementById("transaction-form").addEventListener("submit", function 
   const date = document.getElementById("date-input").value;
   const type = document.querySelector('input[name="type-input"]:checked').value;
 
-  
-
   //  adicionar lançamento
   data.transactions.unshift({
     value: value,
@@ -29,11 +26,11 @@ document.getElementById("transaction-form").addEventListener("submit", function 
     date: date,
   });
 
-    saveData(data);
-    event.target.reset();
-    myModal.hide();
+  saveData(data);
+  event.target.reset();
+  myModal.hide();
 
-    alert("Lançamento adicionado com sucesso!");
+  alert("Lançamento adicionado com sucesso!");
 });
 
 // Verificar se está logado, caso não esteja, redirecionar para index.html
@@ -52,7 +49,8 @@ function checkLogged() {
   if (dataUser) {
     data = JSON.parse(dataUser);
   }
-  console.log(data);
+  //   console.log(data);
+  getCashIn();
 }
 
 //Logout do usuário
@@ -66,3 +64,54 @@ logout.addEventListener("click", function () {
 function saveData(data) {
   localStorage.setItem(data.login, JSON.stringify(data));
 }
+//Função para trazer as entradas do tipo 1
+function getCashIn() {
+  const transactions = data.transactions;
+  const cashIn = transactions.filter((item) => item.type === "1");
+  //   console.log(cashIn);
+
+  // Mostrar na tela
+  if (cashIn.length) {
+    let cashInHtml = ``;
+    let limit = 0;
+
+    if (cashIn.length > 5) {
+      limit = 5;
+    } else {
+      limit = cashIn.length;
+    }
+
+    for (let i = 0; i < limit; i++) {
+      //   console.log(i);
+      //   console.log(cashIn[i]);
+
+      const date = new Date(cashIn[i].date);
+      const formattedDate = date.toLocaleDateString("pt-BR", {
+        day: "2-digit",
+        month: "2-digit",
+        year: "numeric",
+      });
+      cashInHtml += `
+            <div class="row mb-4">
+                <div class="col-12">
+                <h3 class="fs-2">R$ ${cashIn[i].value.toFixed(2)}</h3>
+                <div class="container p-0">
+                    <div class="row">
+                    <div class="col-12 col-md-8">
+                        <p>${cashIn[i].description}</p>
+                    </div>
+                    <div class="col-12 col-md-3 d-flex justify-content-end">
+                        <span>${formattedDate}
+                        </span>
+                    </div>
+                    </div>
+                </div>
+                </div>
+            </div>
+        `;
+    }
+    document.getElementById("cash-in-list").innerHTML = cashInHtml;
+  }
+}
+
+// Função para Mostrar transações na tela
