5d17932be1c91ea0cab6f79181f121c1609c81f9
emersonpessoa <eps011108@gmail.com>
2025-11-17 23:06:20 -0300

    feat: Adicionado função para redirecionar à página transactions :tada:

--- public/js/home.js
+++ public/js/home.js
@@ -7,6 +7,9 @@ let data = {
 };
 
 const logout = document.getElementById("button-logout");
+document.getElementById("transactions-button").addEventListener("click", function(){
+  window.location.href = "transactions.html";
+});
 
 checkLogged();
 
